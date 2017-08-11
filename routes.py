c8fb999e844db799f72d2e9e0d79d4e594c5a5a2
Geoffrey Kip <g_kip@live.com>
2017-08-11 00:06:50 -0400

    Working on signup form

--- routes.py
+++ routes.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, session,redirect, url_for
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgressql://localhost/geoffrey.kip'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/geoffrey.kip'
 db.init_app(app)
 
-app.secret_key= "development-key"
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -17,16 +17,27 @@ def index():
 def about():
   return render_template("about.html")
 
-@app.route("/signup",methods=['GET', 'POST'])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    form= SignupForm()
-    if request.method == 'POST';
-       return "Success!"
+  form = SignupForm()
 
-    elif request.method == "GET";
-       return render_template("signup.html", form=form)
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
 
+      session['email']= newuser.email
+      return redirect(url_for('home'))
 
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
